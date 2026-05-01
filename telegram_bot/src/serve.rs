3377c5f84347b41dff9bf7fc2a5fc340cfa5eb0f
Anuchit Chalothorn <anoochit@gmail.com>
2026-05-01 09:14:52 +0700

    move telegram bot to https://github.com/anoochit/telegram_bot

--- telegram_bot/src/serve.rs
+++
@@ -1,8 +0,0 @@
-use adk_rust::Agent;
-use adk_rust::Launcher;
-use std::sync::Arc;
-
-pub(crate) async fn run_serve(agent: Arc<dyn Agent>) -> anyhow::Result<()> {
-    Launcher::new(agent).run_serve_directly(8080).await?;
-    Ok(())
-}
